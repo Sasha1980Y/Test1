c0d576d82c60e565de43f964b4e64e11673872c9
AlexanderYakovenko <AlexanderYakovenko@Hellens-Mac-mini.local>
2017-05-29 10:57:36 +0300

    First screen

--- Test1/ViewController.swift
+++ Test1/ViewController.swift
@@ -9,10 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var enterYourLogin: UITextField!
+    
+    @IBOutlet weak var enterYourPassword: UITextField!
+    
+    @IBAction func signIn(_ sender: Any) {
+    }
 
+    @IBAction func signUp(_ sender: Any) {
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // keyboards is open
+        enterYourLogin.keyboardType = UIKeyboardType.asciiCapable
+        enterYourLogin.becomeFirstResponder()
+        
+        // hidden navigation Bar
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        
+        // replace "Return" -> "Done"
+        enterYourLogin.returnKeyType = UIReturnKeyType.done
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
